95f60f09a4f4ea7669c87c803c3081a9f637e653
Pridestike <70127247+Pridestike@users.noreply.github.com>
2022-10-11 05:03:38 +0700

    Update hwid.cpp

--- Source C++/hwid.cpp
+++ Source C++/hwid.cpp
@@ -130,17 +130,63 @@ size_t EntryPoint(void* ntoskrn, void* image, void* alloc)
 	return 0;
 }*/
 
-extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT object, PUNICODE_STRING registry)
+struct REQUEST_STRUCT
 {
-	UNREFERENCED_PARAMETER(object);
-	UNREFERENCED_PARAMETER(registry);
+	PIO_COMPLETION_ROUTINE OldRoutine;
+	PVOID OldContext;
+	ULONG OutputBufferLength;
+	PVOID SystemBuffer;
+};
 
+NTSTATUS completed_storage_query(
+	PDEVICE_OBJECT device_object,
+	PIRP irp,
+	PVOID context
+)
+{
+	if(!context)
+	{
+		KdPrint(("%s %d : Context was nullptr\n", __FUNCTION__, __LINE__));
+		return STATUS_SUCCESS;
+	}
 
-	auto        status = STATUS_SUCCESS;
-	UNICODE_STRING  drv_name;
+	const auto request = (REQUEST_STRUCT*)context;
+	const auto buffer_length = request->OutputBufferLength;
+	const auto buffer = (PSTORAGE_DEVICE_DESCRIPTOR)request->SystemBuffer;
+	const auto old_routine = request->OldRoutine;
+	const auto old_context = request->OldContext;
+	ExFreePool(context);
 
-	RtlInitUnicodeString(&drv_name, L"\\Driver\\MyFirstIoctlPastedSpoofer");
-	status = IoCreateDriver(&drv_name, &HookedDriver);
+	do
+	{
+
+		if(buffer_length < FIELD_OFFSET(STORAGE_DEVICE_DESCRIPTOR, RawDeviceProperties))
+			break;	// They just want the size
+
+		if(buffer->SerialNumberOffset == 0)
+		{
+			KdPrint(("%s %d : Device doesn't have unique ID\n", __FUNCTION__, __LINE__));
+			break;
+		}
+
+		if(buffer_length < FIELD_OFFSET(STORAGE_DEVICE_DESCRIPTOR, RawDeviceProperties) + buffer->RawPropertiesLength
+			|| buffer->SerialNumberOffset < FIELD_OFFSET(STORAGE_DEVICE_DESCRIPTOR, RawDeviceProperties)
+			|| buffer->SerialNumberOffset >= buffer_length
+			)
+		{
+			KdPrint(("%s %d : Malformed buffer (should never happen) size: %d\n", __FUNCTION__, __LINE__, buffer_length));
+		}
+		else
+		{
+			const auto serial = (char*)buffer + buffer->SerialNumberOffset;
+			KdPrint(("%s %d : Serial0: %s\n", __FUNCTION__, __LINE__, serial));
+
+		}
+	} while(false);
+
+	// Call next completion routine (if any)
+	if(irp->StackCount > 1ul && old_routine)
+		return old_routine(device_object, irp, old_context);
 
 	return STATUS_SUCCESS;
 }
